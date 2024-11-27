fd534bebd636ae0e5894e9a01a3cfaa3eb29fc13
fahmed <fahmed7921@gmail.com>
2024-11-26 21:21:05 -0600

    last chapter

--- js_practice/chapter_5/sandbox.js
+++ js_practice/chapter_5/sandbox.js
@@ -7,20 +7,15 @@
 //     location: 'Dhaka',
 //     blogs: ['Why mac and cheese rules', '10 things to do with Fahmed']
 // };
-
 // console.log(user);
 // console.log(user.name); // dot notation
-
-// // user.age = 35;
+// user.age = 35;
 // console.log(user.age);
-
 // console.log(user['email']);
 // user['name'] = 'anu';
 // console.log(user['name']);
-
 // const key = 'location';
 // console.log(user[key])
-
 // console.log(typeof user); //type of
 
 // how to add method in objects -------------------------------------
@@ -51,6 +46,9 @@
 // user.login();
 // user.logout();
 // user.logBlogs();
+// console.log(user.name);
+// user.name = 'haha';
+// console.log(user.name);
 
 
 //Array can also have objects stored into them
@@ -58,6 +56,7 @@
 //     {title: 'Why mac and cheese rules', likes: 30},
 //     {title: '10 things to do with Fahmed', likes: 50},
 // ];
+
 // console.log(blogs)
 
 // Math object -- pre-made
@@ -72,11 +71,34 @@
 // console.log(Math.ceil(area)); // largest int
 // console.log(Math.trunc(area)); // only int
 
-// // random numbers generation
+// random numbers generation
 // const random = Math.random();
 // console.log(random); // any random between 0 - 1
 // console.log(Math.round(random*100)); //random between 1-100
 
 
+// primitive values
 
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
 
+// console.log(
+//     `scoreOne: ${scoreOne}`,
+//     `scoreTwo: ${scoreTwo}`
+// )
+// scoreOne = 100;
+// console.log(
+//     `scoreOne: ${scoreOne}`,
+//     `scoreTwo: ${scoreTwo}`
+// )
+
+//reference values
+// const userOne={
+//     name: 'ryu',
+//     age: 30
+// };
+// const userTwo = userOne;
+// console.log(userOne, userTwo);
+// userOne.age = 40;
+// userOne.name = 'Fahmed'
+// console.log(userOne, userTwo);
---
+++ js_practice/chapter_6/sandbox.js
@@ -0,0 +1,127 @@
+//grab single element
+// const para = document.querySelector('.error');
+// console.log(para);
+// const param = document.querySelector('div.error');
+// console.log(param);
+// const parame = document.querySelector('body > h1');
+// console.log(parame);
+
+//grab multiple elements
+// const paras = document.querySelectorAll('p')
+// console.log(paras)
+// console.log(paras[0])
+// console.log(paras[1])
+// paras.forEach(para =>{
+//     console.log(para);
+// });
+// const errors= document.querySelectorAll('.error');
+// console.log(errors)
+
+// get an element by ID -----------------------------------------
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get an element by their class name -----------------------------------------
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+// get an element by their tag name -----------------------------------------
+// const paras = document.getElementsByTagName('p')
+// console.log(paras);
+// console.log(paras[0]);
+// console.log(paras[1]);
+// console.log(paras[2]);
+
+//change para information
+// const para= document.querySelector('p');
+
+// console.log(para.innerText);
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome';
+
+//change multiple para info
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para =>{
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+//     // console.log(para)
+// })
+// for divs
+// const content = document.querySelector('.content');
+// // console.log(content.innerHTML);
+// content.innerHTML += '<h2>This is a new h2</h2>'// if you want to appeand
+// content.innerHTML = '<h2>This is a new h2</h2>'
+
+////for arrays
+// const content = document.querySelector('.content');
+// const people = ['fahmed', 'anu', 'rashid'];
+// people.forEach(person =>{
+//     content.innerHTML += `<p>${person}</p>`;
+// });
+
+// for a tags 
+// const link= document.querySelector('a')
+// console.log (link.getAttribute('href'))// arrtirbute and arguments are needed.
+// link.setAttribute('href', 'https://www.thenetninja.co.uk')
+// link.innerText = 'The Net ninja website'
+
+// const mssg = document.querySelector('p');
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class', 'succuss');
+// console.log(mssg.getAttribute('class'));
+
+// mssg.setAttribute('style', 'color: green')// setting a style on a single tag
+
+//if try to add a new style attribute
+
+// const title = document.querySelector('h1')
+
+// // title.setAttribute('style', 'margin: 50px')// this overwirtes the previous style. 
+// console.log(title.style);
+// console.log(title.style.color); //to check the color in console
+
+// title.style.margin = '50px'; // this just added the margin keeping the previous css intact
+// title.style.color = 'crimson'; //updating the exisiting color
+// title.style.fontSize= '60px'; // in css it is font-size but in js its camelcase such as fontSize
+// title.style.margin= '';
+
+//add and remove classes from elements using style.css
+// const content = document.querySelector('p');
+// console.log(content.classList);
+// content.classList.add('errors')//adding a class
+// content.classList.remove('error')//remove a class
+// content.classList.add('succuess')//remove a class
+
+//practice to add error and success tags on p
+const param = document.querySelectorAll('p');
+console.log(param.classList);
+param.forEach(para=>{
+    console.log(para)
+});
+param[0].classList.add('error')
+param[1].classList.add('success')
+// param[2].classList.length === 0;
+const abc = param[2].classList.length;
+if(abc !== 0){
+    console.log('This has a value');
+}else{
+    param[2].classList.add('lorem');
+    console.log(param[2]);
+}
+param[3].classList.add('success')
+param[4].classList.add('error')
+param[5].classList.add('lorem')
+param[6].classList.add('error')
+param[7].classList.add('success')
+
+
+
+
+
+
+
+
+
+
+
